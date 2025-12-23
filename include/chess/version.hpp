da2e7418ded09f24edaeff23e036cd9b019ebb17
Guillem Portella <ktxc111@astrazeneca.net>
2025-12-23 23:42:15 +0100

    version update

--- include/chess/version.hpp
+++ include/chess/version.hpp
@@ -6,7 +6,7 @@
 namespace chess {
 
 inline constexpr std::string_view kEngineName = "skaks";
-inline constexpr std::string_view kEngineVersion = "0.6";
+inline constexpr std::string_view kEngineVersion = "0.7";
 
 inline constexpr std::array<std::string_view, 11> kOptimizationFeatures{
     "Bitboard move generation with precomputed attack masks",
@@ -19,7 +19,7 @@ inline constexpr std::array<std::string_view, 11> kOptimizationFeatures{
     "Quiescence search to reduce horizon effect",
     "Killer move heuristic for quiet move ordering",
     "Support for polyglot book of moves",
-    "Null move pruning + historical heuristic",
+    "Null move pruning, historical heuristic and SEE sorting",
 };
 
 } // namespace chess
